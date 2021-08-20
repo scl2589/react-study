76846277931a44fe3f7324012f61fc978694e8ac
scl2589 <chaelinshin96@gmail.com>
2021-08-20 09:01:11 +0900

    Update tours project - display data

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>our tour</h2>
+        <div className='underline'>
+        </div>
+      </div>
+      <div>
+        {tours.map((tour)=> {
+            return (
+              <Tour key={tour.id} {...tour}></Tour>
+            )
+          }
+        )}
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
