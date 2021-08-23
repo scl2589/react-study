6ed4941946e9da7220df7c387c6e2a3429cd28b3
scl2589 <chaelinshin96@gmail.com>
2021-08-24 01:00:33 +0900

    Add Reviews project - design

---
+++ 03-reviews/setup/src/Review.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>
+        Surprise!
+      </button>
+    </article>
+  )
+};
+
+export default Review;
