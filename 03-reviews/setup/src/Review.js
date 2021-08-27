d5f39562a1c85bfea909d8684c53ff03482ec28f
Chae Lin Shin <chaelinshin96@gmail.com>
2021-08-27 16:32:02 +0900

    Update Reviews project - add functions for random person

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -30,6 +30,14 @@ const Review = () => {
     })
   }
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  }
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -49,7 +57,7 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn'>
+      <button className='random-btn' onClick={randomPerson}>
         Surprise!
       </button>
     </article>
