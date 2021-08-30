aa9b27e9481afc64b452efa2c769281f48a4bc9c
scl2589 <chaelinshin96@gmail.com>
2021-08-31 00:43:33 +0900

    Update menu project - dynamic menu categories

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
   return (
     <main>
       <section className='menu section'>
@@ -14,7 +26,7 @@ function App() {
           <div className='underline'>
           </div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu items={menuItems}/>
       </section>
     </main>
