76e855936b7746f111ea31e5647ad5eeb8fe4395
scl2589 <chaelinshin96@gmail.com>
2021-08-30 23:51:46 +0900

    Add menu project and display menu items

---
+++ 05-menu/setup/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(items);
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'>
+          </div>
+        </div>
+        <Categories />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  )
+  }
+export default App;
