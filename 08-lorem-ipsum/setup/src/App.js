888ba261a68bdea4469578b831330bb48c348bde
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-01 20:59:16 +0900

    Add lorem ipsum project - setup structure

---
+++ 08-lorem-ipsum/setup/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  }
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count} onCange={(e)=>setCount(e.target.value)}/>
+        <button type="submit" className="btn">
+          GENERATE
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>Lorem ipsum, dolor sit amet consectetur adipsicing elit. Doloribus, voluptate!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipsicing elit. Doloribus, voluptate!</p>
+      </article>
+    </section>
+  )
+}
+
+export default App;
