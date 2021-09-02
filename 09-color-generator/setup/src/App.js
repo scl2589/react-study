4eb9f1490caf3192f09fa15fa630af020ada3aed
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-02 17:27:54 +0900

    Update color generator - get values and show error

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -9,8 +9,15 @@ function App() {
   const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('hello');
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      console.log(colors)
+    } catch(error) {
+      setError(true);
+      console.log(error);
+    }
+
   }
 
   return (
@@ -18,7 +25,7 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025"/>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" className={`${error ? 'error' : null}`}/>
           <button className="btn" type="submit">submit</button>
         </form>
       </section>
