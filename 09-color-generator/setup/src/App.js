1a659fd425ad6a42a397715759ac6e941c01022a
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-02 17:37:24 +0900

    Update color generate - show colors

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault()
     try {
       let colors = new Values(color).all(10)
-      console.log(colors)
+      setList(colors)
     } catch(error) {
       setError(true);
       console.log(error);
@@ -30,7 +30,10 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index)=>{
+          console.log(color);
+          return <SingleColor key="index" {...color} index={index}/>
+        })}
       </section>
     </>
   )
