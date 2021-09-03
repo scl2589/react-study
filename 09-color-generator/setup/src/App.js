943b4130de809a3dc9b602e923d2302ffc3a3982
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-03 13:50:24 +0900

    Update color generator - generate colors with rgb values

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -31,8 +31,7 @@ function App() {
       </section>
       <section className="colors">
         {list.map((color, index)=>{
-          console.log(color);
-          return <SingleColor key="index" {...color} index={index}/>
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
         })}
       </section>
     </>
