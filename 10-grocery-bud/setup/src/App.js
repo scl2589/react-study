07ecf108f76138c3e69af8a4a60ed98ce63a4b30
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-03 19:57:21 +0900

    Update grocery bud project - add items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -10,7 +10,16 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: '', type:''})
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello')
+    if (!name) {
+      // if the value is empty, display alert 
+    } else if (name && isEditing) {
+      // if something is in a value, and editing is true, -> deal with edit
+    } else {
+      // show alert and create item 
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem])
+      setName('')
+    }
   }
   return (
     <section className="section-center">
@@ -24,12 +33,15 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">
-          clear items
-        </button>
-      </div>
+      { list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list}/>
+          <button className="clear-btn">
+            clear items
+          </button>
+        </div>
+      )}
+
     </section>
   )
 }
