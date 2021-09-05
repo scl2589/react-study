aa687da516ef23c49ff153f0ac50a22dd43c4806
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-05 11:40:47 +0900

    Update Navbar project - add menu from data

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -15,32 +15,25 @@ const Navbar = () => {
       </div>
       <div className="links-container show-container">
         <ul className="links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Products</a>
-          </li>
+          {links.map((link)=>{
+            const {id, url, text} = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
       <ul className="social-icons">
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-          <a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a>
-        </li>
+        {social.map((socialIcon) => {
+          const {id, url, icon} = socialIcon 
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
       </ul>
     </div>
   </nav>
