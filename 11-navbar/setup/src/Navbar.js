fde4634bebc49ea678e120bd7dc46c66f6a15431
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-05 12:00:43 +0900

    Update navbar project - toggle with useRef approach

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,6 +5,18 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height='0px'
+    }
+  }, [showLinks])
+
   return (
     <nav>
       <div className="nav-center">
@@ -17,10 +29,22 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className={`${showLinks ? 'links-container show-container': 'links-container' 
-        }`}
+        {/* <div className={`${showLinks ? 'links-container show-container': 'links-container' 
+          }`}
         >
           <ul className="links">
+            {links.map((link)=>{
+              const {id, url, text} = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div> */}
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link)=>{
               const {id, url, text} = link;
               return (
