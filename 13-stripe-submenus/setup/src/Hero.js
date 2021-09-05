396299b97289de73ffbf6b22668c5b0a53451e47
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-05 15:47:57 +0900

    Update stripe project - add submenu and update its location

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
             Millions of companies of all sizes-from startups to Fortune 500s-
             use Stripe's software and APIs to accept payments, send payouts, and manage their businesses online
           </p>
-          <button class="btn">start now</button>
+          <button className="btn">start now</button>
         </article>
         <article className="hero-images">
           <img src={phoneImg} className="phone-img" alt="phone" />
--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -5,6 +5,13 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+  const displaySubmenu = (e) => {
+    const page = e.target.textContext;
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3
+    openSubmenu(page, { center, bottom })
+  }
   return (
     <nav className="nav">
       <div className="nav-center">
@@ -16,17 +23,17 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">
+            <button className="link-btn" onMouseOver={displaySubmenu}>
               products
             </button>
           </li>
           <li>
-            <button className="link-btn">
+            <button className="link-btn"  onMouseOver={displaySubmenu}>
               developers
             </button>
           </li>
           <li>
-            <button className="link-btn">
+            <button className="link-btn"  onMouseOver={displaySubmenu}>
               company
             </button>
           </li>
