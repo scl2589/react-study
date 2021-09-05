827053301497f23dfc759dcdcacdd851cbd0d3de
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-05 11:59:47 +0900

    Update navbar project - class toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,39 +4,46 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return (
-  <nav>
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src={logo} alt="logo" />
-        <button className="nav-toggle">
-          <FaBars />
-        </button>
-      </div>
-      <div className="links-container show-container">
-        <ul className="links">
-          {links.map((link)=>{
-            const {id, url, text} = link;
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button 
+            className="nav-toggle" 
+            onClick={()=> setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div className={`${showLinks ? 'links-container show-container': 'links-container' 
+        }`}
+        >
+          <ul className="links">
+            {links.map((link)=>{
+              const {id, url, text} = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const {id, url, icon} = socialIcon 
             return (
               <li key={id}>
-                <a href={url}>{text}</a>
+                <a href={url}>{icon}</a>
               </li>
             )
           })}
         </ul>
       </div>
-      <ul className="social-icons">
-        {social.map((socialIcon) => {
-          const {id, url, icon} = socialIcon 
-          return (
-            <li key={id}>
-              <a href={url}>{icon}</a>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
