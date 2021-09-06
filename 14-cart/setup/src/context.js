b6895f41019da12ea30b0d665ccd5d65580cdf4f
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-06 16:19:45 +0900

    Update cart project - remove and increase the amount of the item

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -24,12 +24,22 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'REMOVE', payload: id })
   }
 
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id })
+  }
+
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id })
+  }
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
-        remove
+        remove,
+        increase,
+        decrease
       }}
     >
       {children}
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -4,6 +4,24 @@ const reducer = (state, action) => {
         return {...state, cart: []}
     } else if (action.type === 'REMOVE') {
         return { ...state, cart: state.cart.filter((cartItem) => cartItem.id !== action.payload )}
+    } else if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem
+        })
+        return {...state, cart:tempCart }
+    } else if (action.type === 'DECREASE') {
+        let tempCart = state.cart
+            .map((cartItem) => {
+                if (cartItem.id === action.payload) {
+                    return {...cartItem, amount: cartItem.amount - 1}
+                }
+                return cartItem
+            })
+            .filter((cartItem) => cartItem.amount !== 0)
+        return {...state, cart:tempCart } 
     }
     return state
 }
