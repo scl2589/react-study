4b9f90f1c7a92a56ce2daf1c79dc37fec2ad1a94
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-06 15:57:40 +0900

    Update cart project - remove cart item

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -2,6 +2,8 @@
 const reducer = (state, action) => {
     if (action.type === 'CLEAR_CART') {
         return {...state, cart: []}
+    } else if (action.type === 'REMOVE') {
+        return { ...state, cart: state.cart.filter((cartItem) => cartItem.id !== action.payload )}
     }
     return state
 }
