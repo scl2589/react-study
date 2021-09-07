21e87b5f3ece84e32eee9c511bd5e9b5db5ddc26
scl2589 <chaelinshin96@gmail.com>
2021-09-07 10:19:30 +0900

    Update cocktails project - add about and error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Nulla non ipsum et risus pellentesque volutpat vel semper augue. 
+      Morbi lacinia tincidunt ullamcorper. 
+      Praesent at mauris consequat, posuere est id, efficitur eros. 
+      Phasellus auctor risus at aliquam congue. 
+      Aliquam ornare erat vitae tellus congue pulvinar. 
+      Integer ac convallis felis, a commodo ante. 
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+    </section>
   )
 }
 
