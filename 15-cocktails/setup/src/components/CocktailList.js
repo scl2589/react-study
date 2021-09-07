ef6055223603d39877eaae6d560a8eb60d1a9428
scl2589 <chaelinshin96@gmail.com>
2021-09-07 11:34:43 +0900

    Update cocktails project - set context

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -4,6 +4,22 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        No Cocktails Matched Your Search Criteria
+      </h2>
+    )
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
