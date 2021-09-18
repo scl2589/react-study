3f859d8dd39f6c0681d48945c155f4e60f4442c5
Chae Lin Shin <chaelinshin96@gmail.com>
2021-09-18 10:02:45 +0900

    Add game 2048

---
+++ practical_react_programming/6_game2048/src/util/assert.js
@@ -0,0 +1,6 @@
+export const assert = function (condition, message) {
+    if (!condition) {
+      throw new Error(`Assertion failed: ${message}`);
+    }
+  };
+  
---
+++ practical_react_programming/6_game2048/src/util/number.js
@@ -0,0 +1,4 @@
+export function getRandomInteger(from, to) {
+    return Math.floor(Math.random() * to + from);
+  }
+  
